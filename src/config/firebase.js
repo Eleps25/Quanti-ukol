1dce4491a8d3f4f672d02982a98c38f64a8efd56
Jakub Mezulanik <mezuja@gmail.com>
2023-11-07 20:15:31 +0100

    Added-firebase_reactRouter_reactBootstrap

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB1wFF6aRPgL20SuTR-516wDI6dWzZKRRU",
+  authDomain: "quanti-ukol.firebaseapp.com",
+  projectId: "quanti-ukol",
+  storageBucket: "quanti-ukol.appspot.com",
+  messagingSenderId: "1082734954971",
+  appId: "1:1082734954971:web:52822a9f04a9c49064fffe"
+};
+
+const app = initializeApp(firebaseConfig);
