9fa83eae529940dde4d61b9d6aa5db7433fe0113
Jakub Mezulanik <mezuja@gmail.com>
2023-11-10 17:23:09 +0100

    added sort functionality

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -12,7 +12,7 @@ import {
 import { db } from "../../config/firebase";
 import AddItemForm from "../AddItemForm/AddItemForm";
 
-import  Button  from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button";
 
 export default function ItemList() {
   const [items, setItems] = useState([]);
@@ -20,6 +20,9 @@ export default function ItemList() {
   const [newItemTitle, setNewItemTitle] = useState("");
   const [newItemBody, setNewItemBody] = useState("");
   const [isAddingItem, setIsAddingItem] = useState(false);
+
+  const [isSorted, setIsSorted] = useState(false);
+  const [isAscSorted, setIsAscSorted] = useState(false);
 
   const itemsCollectionRef = collection(db, "items");
 
@@ -81,9 +84,55 @@ export default function ItemList() {
     }
   };
 
+  const sortAsc = (columnToSort, items) => {
+    const sortedItems = items.toSorted((firstItem, secondItem) => {
+        const columnOne = firstItem[columnToSort].toUpperCase();
+        const columnTwo = secondItem[columnToSort].toUpperCase();
+        if (columnOne < columnTwo) {
+          return -1;
+        }
+        if (columnOne > columnTwo) {
+          return 1;
+        }
+        return 0;
+      });
+    return sortedItems;
+  }
+
+  const sortDesc = (columnToSort, items) => {
+    const sortedItems = items.toSorted((firstItem, secondItem) => {
+        const columnOne = firstItem[columnToSort].toUpperCase();
+        const columnTwo = secondItem[columnToSort].toUpperCase();
+        if (columnOne < columnTwo) {
+          return 1;
+        }
+        if (columnOne > columnTwo) {
+          return -1;
+        }
+        return 0;
+      });
+    return sortedItems;
+  }
+
+
+  const sortItems = () => {
+    if (!isSorted) {
+      setItems(sortAsc("title", items));
+      setIsSorted(true);
+      setIsAscSorted(true);
+    } else if (isSorted && isAscSorted) {
+      setItems(sortDesc("title", items));
+      setIsAscSorted(false);
+    } else {
+      setItems(sortAsc("id", items));
+      setIsSorted(false);
+    }
+  };
+
   return (
     <div>
       <h1>Item List</h1>
+      <Button onClick={sortItems}>Sort Items</Button>
       {items.map((item) => (
         <Item
           item={item}
@@ -92,8 +141,17 @@ export default function ItemList() {
           deleteItem={() => deleteItem(item.id)}
         />
       ))}
-      <Button variant="primary" onClick={() => setIsAddingItem(true)}>Add new Item</Button>
-      {isAddingItem && <AddItemForm addItem={addItem} setTitle={setNewItemTitle} setBody={setNewItemBody} stopAddingItem={() => setIsAddingItem(false)}/>}
+      <Button variant="primary" onClick={() => setIsAddingItem(true)}>
+        Add new Item
+      </Button>
+      {isAddingItem && (
+        <AddItemForm
+          addItem={addItem}
+          setTitle={setNewItemTitle}
+          setBody={setNewItemBody}
+          stopAddingItem={() => setIsAddingItem(false)}
+        />
+      )}
     </div>
   );
 }
