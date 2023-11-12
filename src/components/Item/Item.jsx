737f52504de3fb6d1e4785fcce383162bd9b8cac
Jakub Mezulanik <mezuja@gmail.com>
2023-11-12 14:14:56 +0100

    Item-start styles

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,22 +1,27 @@
-import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
+
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import "./style.css";
 
 export default function Item(props) {
   const { id, title, body, isImportant } = props.item;
   return (
-    <div>
-      <h2>
-        <Link to={`/itemlist/${id}`}>Item</Link> {id}:{" "}
-        {isImportant ? "is important" : "not important"}
-      </h2>
-      <h3>{title}</h3>
-      <p>{body}</p>
-      <input
-        onChange={props.updateItemImportant}
-        type="checkbox"
-        checked={isImportant}
-      />
-      <Button onClick={props.deleteItem} variant="danger">Delete</Button>
-    </div>
+    <Card>
+      <Card.Body>
+        <Card.Title className="myCard-title">
+          <Link to={`/itemlist/${id}`}>{title}</Link>
+          <input
+          onChange={props.updateItemImportant}
+          type="checkbox"
+          checked={isImportant}
+        />
+        </Card.Title>
+        <Card.Text>{body}</Card.Text>
+        <Button onClick={props.deleteItem} variant="danger">
+          Delete
+        </Button>
+      </Card.Body>
+    </Card>
   );
 }
